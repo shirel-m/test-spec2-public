f892b01583eca44b8407bb88ba8bf8447efcb1f0
shirel.m <shirel.m@quali.com>
2025-03-23 13:46:21 +0200

    removed comment

--- terraform/s3/main.tf
+++ terraform/s3/main.tf
@@ -36,8 +36,6 @@ resource "aws_iam_policy" "policy" {
   path        = "/"
   description = "Policy to access S3 Module"
 
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
   policy = jsonencode({
     Version: "2012-10-17",
     Statement: [
